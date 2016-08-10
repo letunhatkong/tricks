17166d468ad076bc3ca258d5205baa1e23c9dff9
letunhatkong <letunhatkong@gmail.com>
2016-08-10 14:29:36 +0700

    abc

---
+++ share social.php
@@ -0,0 +1,19 @@
+Creating share buttons with just URL's
+
+Twitter
+
+http://twitter.com/share?text=<TITLE>&url=<URL>
+
+E.g. http://twitter.com/share?text=This+is+google+a+search+engine&url=https%3A%2F%2Fwww.google.com
+
+Facebook
+
+http://www.facebook.com/sharer.php?u=<URL>&p[title]=<TITLE>
+
+E.g. http://www.facebook.com/sharer.php?u=https%3A%2F%2Fwww.google.com%[title]=This+Is%2C+Google+a+search+engine
+
+Pinterest
+
+http://pinterest.com/pin/create/button/?url=<URL>&description=<TITLE>
+
+E.g http://pinterest.com/pin/create/button/?url=https%3A%2F%2Fwww.google.com&description=This+is+google+a+search+engine
