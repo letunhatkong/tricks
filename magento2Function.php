43f617c45a730f29fc502ef17be8f02512597170
letunhatkong <letunhatkong@gmail.com>
2016-08-22 09:36:48 +0700

    22

--- magento2Function.php
+++ magento2Function.php
@@ -254,7 +254,7 @@ INSTALL PHP 5.6.x
 sudo apt-get -y update
 sudo add-apt-repository ppa:ondrej/php5-5.6
 sudo apt-get -y update
-sudo apt-get -y install php5 php5-common php5-mcrypt php5-curl php5-cli php5-mysql php5-gd php5-intl php5-xsl libapache2-mod-php5 libcurl3 php5-imagick
+sudo apt-get -y install php5 php5-common php5-mcrypt php5-curl php5-cli php5-mysql php5-gd php5-intl php5-xsl libapache2-mod-php5 libcurl3 php5-imagick zip
 sudo apt-get install mysql-server-5.6 php5-mysql
 
 INSTALL imagemagick
